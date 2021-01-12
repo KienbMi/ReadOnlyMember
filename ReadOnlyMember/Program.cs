b08908a6221a791217891a53f8351da5c34261d7
KienbMi <m_kienberger@gmx.net>
2021-01-12 19:26:48 +0100

    Add project files.

---
+++ ReadOnlyMember/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReadOnlyMember
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            await foreach (var number in GenerateSequence())
+            {
+                Console.WriteLine(number);
+            }
+
+
+            static async IAsyncEnumerable<int> GenerateSequence()
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    await Task.Delay(100);
+                    yield return i;
+                }
+            }
+
+        }
+    }
+
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+
+        public readonly override string ToString() =>
+            $"({X}, {Y}) is {Distance} from the origin";
+
+        public void Translate(int xOffset, int yOffset)
+        {
+            X += xOffset;
+            Y += yOffset;
+        }
+    }
+}
